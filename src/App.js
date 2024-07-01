297174ac6a4597271e0db608a3f031198c57cc8d
meena-rathi Maheshwari <meenakumari16@hotmail.com>
2024-07-01 16:33:46 +0000

    pi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+
+import Navv from "./Components/Navv";
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className={styles.App}>
+    
+      <Navv />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm />} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
